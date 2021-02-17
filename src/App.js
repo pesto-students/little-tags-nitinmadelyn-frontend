55b65d952086b43c75c1b98d73303aaec980f22c
nitinmadelyn <nitin.madelyn@gmail.com>
2021-02-17 18:29:23 +0530

    init react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Little Tag</h1>
+        <p>One stop shop for clothing</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
